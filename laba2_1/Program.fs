0f80e85e0a8ecee7c5db91bdb253cfd85a61b3e5
unknown <balabanovsash@gmail.com>
2026-03-02 22:39:33 +0500

    first commit

---
+++ laba2_1/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let rec MinNum num min = 
+    if num < 10 then
+        if num < min then
+            num
+        else
+            min
+    else
+        if num % 10 < min then
+            MinNum (num/10) (num%10)
+        else
+            MinNum (num/10) min
+
+let FindMinNum numbers = 
+    [
+        for i = 0 to List.length numbers - 1 do
+            MinNum numbers.[i] 10
+    ]
+
+[<EntryPoint>]
+let main args = 
+    printf "Введите кол-во эл списка: "
+    let count_el = int(Console.ReadLine())
+    printfn "Введите эл списка: "
+    let numbers = [for i in 0..count_el-1 -> int(Console.ReadLine())]
+    let list_num = FindMinNum numbers
+    printfn "Исходные числа: %A" numbers
+    printfn "Минимальные цифры: %A" list_num
+    0
---
+++ laba2_2/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let rec RecNum num required_number = 
+    if num < 10 then
+        if num = required_number then
+            1
+        else
+            0
+    else
+        if num % 10 = required_number then
+            1
+        else
+            RecNum (num/10) required_number
+
+let FindNum numbers required_number = 
+    [
+        for i = 0 to List.length numbers - 1 do
+            RecNum numbers.[i] required_number
+    ]
+
+[<EntryPoint>]
+let main args = 
+    printf "Введите кол-во эл списка: "
+    let count_el = int(Console.ReadLine())
+    printfn "Введите эл списка: "
+    let numbers = [for i in 0..count_el-1 -> int(Console.ReadLine())]
+    printf "Какое число необходимо найти: "
+    let required_number = int(Console.ReadLine())
+    let list_num = FindNum numbers required_number
+    let sum = List.fold (fun acc x -> acc + x) 0 list_num
+    printfn "Кол-во чисел с %i: %i" required_number sum
+    0
